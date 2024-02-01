d3fe44c67c16857a24df28e339a42d21c7b8d18a
vondia <pimvdbosch@gmail.com>
2024-02-01 14:13:49 +0100

    setup panda CSS and added data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,103 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { css } from '../../styled-system/css';
+import { promises as fs } from 'fs';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+type TeamId = number;
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+type TeamIdToName = {
+  [key in TeamId]: string;
+};
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+type Game = {
+  awayTeamId: TeamId;
+  homeTeamId: TeamId;
+  awayScore: number;
+  homeScore: number;
+  weekIndex: number;
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+// Adjusted type for weekData
+type WeekData = Game[];
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+export default async function Home() {
+    const file = await fs.readFile(process.cwd() + '/data/schedules.json', 'utf8');
+    const data = JSON.parse(file);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    const teamIdToName: TeamIdToName = {
+        790364196: 'Vikings',
+        790364172: 'Colts',
+        790364194: 'Texans',
+        790364181: 'Jaguars',
+        790364195: 'Titans',
+        790364161: 'Bears',
+        790364193: 'Steelers',
+        790364173: 'Commanders',
+        790364162: 'Bengals',
+        790364176: 'Eagles',
+        790364175: 'Dolphins',
+        790364163: 'Bills',
+        790364191: "Saints",
+        790364174: 'Cowboys',
+        790364165: 'Browns',
+        790364190: 'Ravens',
+        790364186: 'Panthers',
+        790364171: 'Chiefs',
+        790364166: 'Buccaneers',
+        790364164: 'Broncos',
+        790364187: 'Patriots',
+        790364192: 'Seahawks',
+        790364182: 'Jets',
+        790364160: '49ers',
+        790364188: 'Raiders',
+        790364170: 'Chargers',
+        790364179: 'Giants',
+        790364177: 'Falcons',
+        790364185: 'Packers',
+        790364183: 'Lions',
+        790364167: 'Cardinals',
+        790364189: 'Rams',
+      };
+
+      const getTeamNameById = (teamId: TeamId): string => {
+        return teamIdToName[teamId] || `Unknown Team (${teamId})`;
+      };
+
+    return (
+        <>
+            <div
+                className={css({
+                    mt: "4",
+                    fontSize: "xl",
+                    fontWeight: "semibold",
+                    bg: { base: "gray.50", _hover: "gray.200" },
+                    lg: { fontSize: "2xl", },
+                })}
+            >
+                Panda CSS is the best.
+            </div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Week</th>
+                            <th>Away Team</th>
+                            <th>Home Team</th>
+                            <th>Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data?.reg.map((weekData: WeekData) => (
+                            weekData && weekData.map((game: Game, j: number) => (
+                                <tr key={j}>
+                                    {j === 0 && <td rowSpan={weekData.length} className={css({ verticalAlign: 'top', textAlign: 'center' })}>{game.weekIndex + 1}</td>}
+                                    <td>{getTeamNameById(game.awayTeamId)}</td>
+                                    <td>{getTeamNameById(game.homeTeamId)}</td>
+                                    <td>{`${game.awayScore} - ${game.homeScore}`}</td>
+                                </tr>
+                            ))
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
 }
